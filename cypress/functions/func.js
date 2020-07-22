14446ef4b275a4d0165d948e444e635f35783891
sarmad raees <sarmad.khan@emumba.com>
2020-07-22 10:08:53 +0500

    added my project

--- cypress/functions/func.js
+++ cypress/functions/func.js
@@ -1,8 +1,9 @@
 export function navigate() {
-    cy.visit('https://google.com')
+    cy.visit('https://google.com.pk')
   }
   
   export function search(searchText) {
     cy.get('.gLFyf').type(searchText + '{enter}')
   }
   
+  //trying for fun
