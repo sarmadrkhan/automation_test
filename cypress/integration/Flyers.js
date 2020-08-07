0a3c15a7df2a7872ea9c75e3e7cb86507c6c7a7f
sarmad raees <sarmad.khan@emumba.com>
2020-08-07 15:48:19 +0500

    fixed minor issues

--- cypress/integration/Flyers.js
+++ cypress/integration/Flyers.js
@@ -9,50 +9,60 @@ import {
   import existing_user from '../page_objects/existing_user_form'
   import new_flyer from '../page_objects/new_flyer'
   // * Data *
-var firstname='Mandeep Dhillon';
-var lastname='Sandy';
-
+var firstname;
+var lastname;
+var aNewVar;
   describe('Flyers', () => {
     it('should do the task 2 of the assingment',()=>{
       cy.fixture('myDataFile.json').then((myDataFile)=>{
-        navigate(myDataFile[3].value)
-        flyer_home.clickLoginButton()
-        existing_user.clickNewUserButton()
-        new_user.typerFirstName(firstname)
-        new_user.typeLastName(lastname)
-        new_user.typeEmail(myDataFile[4].value)
-        new_user.typePassword(myDataFile[5].value)
-        new_user.typeConfirmPassword(myDataFile[5].value)
-        new_user.clickExistingUserButton()
-        existing_user.typeEmail(myDataFile[4].value) //username
-        existing_user.typePassword(myDataFile[5].value)
-        existing_user.clickSignInButton()
-        flyer_home.clickPostFlyerButton()
-        // //var a = cy.get('.MuiBox-root > .MuiTypography-h6').text()
-        // //.type(`${a}`)
-        
-        //title
-        new_flyer.copyAndPasteTitle()
-        
-        //description
-        var genArr=range(1,9,2)
-        new_flyer.copyAndPasteDescription(genArr)
-        //scroll to bottom to view the last line
-        new_flyer.scrollDown()
- 
-        new_flyer.typePhone(myDataFile[6].value) //phone number
-        new_flyer.typeAddress(myDataFile[7].value) //address
-        
-        new_flyer.addTag(myDataFile[8].value)  //tag1
-        new_flyer.addTag(myDataFile[9].value)  //tag2
-        new_flyer.addTag(myDataFile[10].value) //tag3
+        cy.fixture('myNewCast.json').then((newCast)=>{
+           aNewVar=newCast[4]
+           cy.log(typeof(newCast[4].Name)) 
+           firstname=aNewVar.Name
+           cy.log(typeof(newCast[4][`Character Name`]))
+            lastname=aNewVar[`Character Name`]
+            navigate(myDataFile[3].value)
+            flyer_home.clickLoginButton()
+            existing_user.clickNewUserButton()
+            new_user.typeFirstName(firstname)
+            new_user.typeLastName(lastname)
+            new_user.typeEmail(myDataFile[4].value)
+            new_user.typePassword(myDataFile[5].value)
+            new_user.typeConfirmPassword(myDataFile[5].value)
+            new_user.clickExistingUserButton()
+            existing_user.typeEmail(myDataFile[4].value) //username
+            existing_user.typePassword(myDataFile[5].value)
+            existing_user.clickSignInButton()
+            flyer_home.clickPostFlyerButton()
+            // //var a = cy.get('.MuiBox-root > .MuiTypography-h6').text()
+            // //.type(`${a}`)
+            
+            //title
+            new_flyer.copyAndPasteTitle()
+            
+            //description
+            var genArr=range(1,9,2)
+            new_flyer.copyAndPasteDescription(genArr)
+            //scroll to bottom to view the last line
+            new_flyer.scrollDown()
+     
+            new_flyer.typePhone(myDataFile[6].value) //phone number
+            new_flyer.typeAddress(myDataFile[7].value) //address
+            
+            new_flyer.addTag(myDataFile[8].value)  //tag1
+            new_flyer.addTag(myDataFile[9].value)  //tag2
+            new_flyer.addTag(myDataFile[10].value) //tag3
+    
+            new_flyer.post()
+            new_flyer.clickHomeButton() //go to home page
+            cy.wait(10000)
+            cy.reload()
+            cy.wait(3000)
+            cy.screenshot('Post a flyer')
 
-        new_flyer.post()
-        new_flyer.clickHomeButton() //go to home page
-        cy.wait(10000)
-        cy.reload()
-        cy.wait(3000)
-        cy.screenshot('Post a flyer')
+
+          })
+        
       })  
     })
   })
